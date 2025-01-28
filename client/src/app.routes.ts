8cb9e4781b091ccbec1eef764a4bdf8d465de3b4
Fabio Paolantonio <fabiopaolantonio@gmail.com>
2025-01-28 11:18:32 +0100

    removed unused components

--- client/src/app.routes.ts
+++ client/src/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages-ex/dashboard/dashboard';
-import { Documentation } from './app/pages-ex/documentation/documentation';
-import { Landing } from './app/pages-ex/landing/landing';
-import { Notfound } from './app/pages-ex/notfound/notfound';
+import { Notfound } from './app/pages/notfound/notfound';
 import { HomePage } from './app/pages/home/home.component';
 import { Logout } from './app/pages/auth/logout';
 
@@ -17,17 +14,6 @@ export const appRoutes: Routes = [
             { path: 'logout', component: Logout}
         ]
     },
-    {
-        path: 'demo',
-        component: AppLayout,
-        children: [
-            { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages-ex/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages-ex/pages.routes') }
-        ]
-    },
-    { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
     { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
