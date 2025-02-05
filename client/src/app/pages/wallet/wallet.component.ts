53198e895b902c91528e1d25f883078410b483e5
Fabio Paolantonio <fabiopaolantonio@gmail.com>
2025-02-05 14:41:23 +0100

    added publish succes and error messages

--- client/src/app/pages/wallet/wallet.component.ts
+++ client/src/app/pages/wallet/wallet.component.ts
@@ -7,16 +7,20 @@ import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'rea-wallet',
-  imports: [CommonModule, FormsModule, InputNumberModule, InputGroupModule, InputGroupAddonModule, ButtonModule, DatePipe],
+  imports: [CommonModule, FormsModule, InputNumberModule, InputGroupModule, InputGroupAddonModule, ButtonModule, DatePipe, ToastModule],
   templateUrl: './wallet.component.html',
-  styleUrl: './wallet.component.scss'
+  styleUrl: './wallet.component.scss',
+  providers: [MessageService]
 })
 export class WalletPage {
   walletService = inject(WalletService);
   authService = inject(AuthService);
+  messageService = inject(MessageService);
 
   accountId = this.authService.getUserId();
 
@@ -45,10 +49,16 @@ export class WalletPage {
       this.walletService.rechargeWallet({ walletId: this.wallet.walletId, amount: this.amount }).subscribe((response) => {
         this.wallet = response;
         this.amount = 50;
+        this.showMessage('success', 'Wallet ricaricato con successo', 'Ricarica effettuata');
       }, (error) => {
+        this.showMessage('error', 'Errore durante la ricarica del wallet', 'Errore');
         this.error = error;
       });
     }
+  }
+
+  showMessage(severity: string='success', message: string='Message Content', summary: string='Success Message') {
+    this.messageService.add({ severity: severity, summary: summary, detail: message, key: 'br', life: 3000 });
   }
 
 
